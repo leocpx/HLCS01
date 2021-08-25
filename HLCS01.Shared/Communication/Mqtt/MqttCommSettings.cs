824d6f9d459cd6191eb3d79b03096305215b1bba
LATA <l.atanasoaeei@osai-as.it>
2021-08-25 10:33:44 +0200

    updated ip

--- HLCS01.Shared/Communication/Mqtt/MqttCommSettings.cs
+++ HLCS01.Shared/Communication/Mqtt/MqttCommSettings.cs
@@ -11,7 +11,7 @@ namespace HLCS01.Shared.Communication
         public static string ClientName { get; set; } = "DummyMqttClient";
         public static string ClientTopic { get; set; } = "R0013";
         public static string SupervisorTopic { get; set; } = "R0013-Supervisor";
-        public static string ServerAddress { get; set; } = "172.18.4.138";
+        public static string ServerAddress { get; set; } = "172.18.4.11";
         public static int ServerPort { get; set; } = 1883;
         public static string User { get; set; } = "minotauro";
         public static string Key { get; set; } = "minotauro";
